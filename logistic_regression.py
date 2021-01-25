75e5273001bf9ed6806e0b9b65420340bf94563a
sumit <mandal.sumit00@gmail.om>
2021-01-25 18:05:13 +0530

    18:04/25-01-21

--- logistic_regression.py
+++ logistic_regression.py
@@ -2,6 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 n_pts=100
+np.random.seed(0)
 top_region = np.array([np.random.normal(10,2,n_pts),np.random.normal(12,2,n_pts)]).T
 bottom_region = np.array([np.random.normal(5,2,n_pts),np.random.normal(6,2,n_pts)]).T
 
@@ -10,5 +11,5 @@ ax.scatter(top_region[:,0],top_region[:,1],color = 'r')
 #The Axes.scatter() function in axes module of matplotlib
 # library is used to plot a scatter of y vs. x with varying
 # marker size and/or color.
-ax.scatter(bottom_region[:,0],top_region[:,1],color = 'b')
+ax.scatter(bottom_region[:,0],bottom_region[:,1],color = 'b')
 plt.show()
