d0bad032572cd81b3c34329d352ba0779fcf3050
sumit <mandal.sumit00@gmail.om>
2021-01-27 20:18:23 +0530

    dd

--- logistic_regression.py
+++ logistic_regression.py
@@ -2,7 +2,9 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 def draw(x1,x2):
-    ln = plt.plot(x1,x2)
+    ln = plt.plot(x1,x2,'-')
+    plt.pause(0.0001)
+    ln[0].remove()
 
 def sigmoid(score):
     return 1 /( 1 + np.exp(-score))
@@ -13,37 +15,52 @@ def calculate_error(line_parameters,points,y):
     cross_entropy = -(1 / m) * (np.log(p).T * y + np.log(1 - p).T * (1 - y))
     return cross_entropy
 
-n_pts=10
+def gradient_descent(line_parameters,points,y,alpha):
+    for i in range(3000):
+        p = sigmoid(points * line_parameters)
+        m = points.shape[0]
+        gradient = (points.T * (p - y) * (alpha / m))
+        line_parameters = line_parameters - gradient
+        w1 = line_parameters.item(0)
+        w2 = line_parameters.item(1)
+        b = line_parameters.item(2)
+
+        x1 = np.array([points[:,0].min(),points[:,0].max()])
+
+        x2 = -b / w2 + x1 * (- w1/w2)
+        draw(x1,x2)
+        print(calculate_error(line_parameters,points,y))
+
+
+n_pts=100
 np.random.seed(0)
 bias = np.ones(n_pts)
 top_region = np.array([np.random.normal(10,2,n_pts), np.random.normal(12,2,n_pts),bias]).T #T means transpose
 bottom_region = np.array([np.random.normal(5,2,n_pts),np.random.normal(6,2,n_pts),bias]).T
 all_points = np.vstack((top_region,bottom_region))
-w1 = -0.2
-w2 = -0.35
-b = 3.5
-line_parameters = np.matrix([w1,w2,b]).T
-x1 = np.array([bottom_region[:,0].min(),top_region[:,0].max()])
 
-x2 = -b / w2 + x1 * (- w1/w2)
+
+line_parameters = np.matrix(np.zeros(3)).T
 
 y = np.array([np.zeros(n_pts),np.ones(n_pts)]).reshape(n_pts*2,1)
-print(y)
+# print(y)
 
-linear_combination = all_points*line_parameters
-linear_combination
-probabilities = sigmoid(linear_combination)
+# linear_combination = all_points*line_parameters
+# linear_combination
+# probabilities = sigmoid(linear_combination)
 
 
 _, ax = plt.subplots(figsize=(4,4))
 ax.scatter(top_region[:,0],top_region[:,1],color = 'r')
+#[ : , 0 ] means (more or less) [ first_row:last_row , column_0 ]
+#this notation will give you all the values in column 0 (from all rows).
 #The Axes.scatter(self,x,y) function in axes module of matplotlib
 # library is used to plot a scatter of y vs. x with varying
 # marker size and/or color.
 #x, y: These parameter are the horizontal and vertical
 #coordinates of the data points.
 ax.scatter(bottom_region[:,0],bottom_region[:,1],color = 'b')
-draw(x1,x2)
+gradient_descent(line_parameters,all_points,y,0.06)
 plt.show()
 
-print(f'error is {calculate_error(line_parameters,all_points,y)}')
+# print(f'error is {calculate_error(line_parameters,all_points,y)}')
